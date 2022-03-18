8ed46a59ac245cef645994384aa5e2baf80c1767
deveckor <deveckor@gmail.com>
2022-03-18 00:17:26 -0600

    Add script card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,28 +4,34 @@ import React from 'react';
 
 function Card(props) {
 
-    const {img} = props;
+    const {img, nameProfile, title, time, tags} = props;
 
+    
     return (
-        <section className="card">
+        <section className="card mt-4 p-4">
             <div className="div-head">
-                <img className="img-profile img-Card" alt="Profile" src={img}/>
+                <img className="img-profile " alt="Profile" src={img}/>
                 <div className="info-card">
-                    <a className="name-profile nameProfile"></a>
-                    <span className="time"></span>
+                    <a className="name-profile">{nameProfile}</a>
+                    <span>{time}</span>
                 </div>
             </div>
             <div className="container-card">
                 <h2 className="sub-title">
-                    <a href="#" className="tittleCard"></a>
+                    <a href="#" >{title}</a>
                 </h2>
             <div className="tags">
+                
+                    {tags.map((tag, index) => {
 
+                        return <a key={index}  className="me-3">{`#${tag}`} </a>;
+                    })}
+                    
             </div>
             <div className="footer-card">
                 <div className="fot-card-left">
-                    <a className="reaction"></a>
-                    <a className="comment"></a>
+                    <a ></a>
+                    <a ></a>
                 </div>
                 <div className="fot-card-right">
                     <button className="btn-save">Delete</button>
--- src/View/Home.js
+++ src/View/Home.js
@@ -1,9 +1,49 @@
-import Card from '../Components/Card'
+import React, {useState, useEffect} from 'react';
+import CardSection from '../Components/CardSection/index'
 
 
-function Home(props) {
-    return (
-        <Card/>
+
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMjZjNzA0NmQ0ZWYzYTQ4YTg3YmVkNCIsImlhdCI6MTY0NzU3ODYwMywiZXhwIjoxNjQ4MTgzNDAzfQ.LLCD6xM_9k1r_hAqiPqTsy2x1J5R1z_XiSIL1OwnU6E'
+const url = 'http://localhost:8080/post/'
+function Home() {
+
+    const [post, setPost] = useState([]);
+    const [login, setLogin] = useState(false);
+
+    useEffect(()=>{
+        let option = {
+            method: 'GET',
+            headers: {
+                "Authorization": token
+            }
+        }
+        const getAll = async ()=>{
+            try {
+                
+                let res = await fetch(url, option);
+                let json = await res.json();
+
+                if(!res.ok) throw {status: res.status, statusText: res.error}
+                let data = json.data.post;
+                setPost(data);
+                setLogin(true)
+                
+            } catch (error) {
+                console.log(error);
+            }
+            
+        }
+        
+        getAll()
+    },[])
+    
+   
+    
+   
+    return(
+        <CardSection
+        post={post}
+        />
     )
 }
 
